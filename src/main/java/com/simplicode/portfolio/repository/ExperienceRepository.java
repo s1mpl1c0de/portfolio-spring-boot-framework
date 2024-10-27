49dd4ec51928c766ff2fdeb7c105c9a1699fc455
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-10-28 01:19:41 +0700

    feat: remove unused code

--- src/main/java/com/simplicode/portfolio/repository/ExperienceRepository.java
+++ src/main/java/com/simplicode/portfolio/repository/ExperienceRepository.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.StringJoiner;
