6964370c23426cbb81c7292dbdf7e90cbc24a8a3
Rujikorn Khampho <jean@Rujikorns-MacBook-Air.local>
2024-11-02 08:55:35 +0700

    feat: add pagination parameters

--- src/main/java/com/simplicode/portfolio/controller/UserController.java
+++ src/main/java/com/simplicode/portfolio/controller/UserController.java
@@ -4,8 +4,11 @@ import com.simplicode.portfolio.dto.request.UserCreateRequest;
 import com.simplicode.portfolio.dto.request.UserUpdateRequest;
 import com.simplicode.portfolio.dto.response.PageNumberPaginationResponse;
 import com.simplicode.portfolio.dto.response.UserResponse;
+import com.simplicode.portfolio.model.Page;
 import com.simplicode.portfolio.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,8 +30,18 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<PageNumberPaginationResponse> findAll() {
-        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    public ResponseEntity<PageNumberPaginationResponse<UserResponse>> findAll(
+       @RequestParam(defaultValue = "1") @Min(1) Integer pageNumber,
+       @RequestParam(defaultValue = "30") @Min(1) Integer pageSize,
+       HttpServletRequest httpRequest
+    ) {
+        Page page = new Page()
+           .setCount(userService.countAll())
+           .setRequestUrl(httpRequest.getRequestURL().toString())
+           .setPageNumber(pageNumber)
+           .setPageSize(pageSize);
+
+        return new ResponseEntity<>(userService.findAll(page), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
